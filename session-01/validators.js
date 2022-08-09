8ba8513414e64d5e016cc48abbed2b9fa2d34119
Coryn Colon <coloncoryn@gmail.com>
2022-08-09 00:19:58 -0400

    [username validator done, password validator 2 failed]

--- session-01/validators.js
+++ session-01/validators.js
@@ -6,18 +6,79 @@
   - username may contain numbers and letters
   - username cannot contain special characters
 */
-function validUsername(username) {
-  return;
+
+function isUserCorrectLength(username){
+  user = username + ""
+  return user.length >= 3 && user.length <= 10;
 }
 
-/*
+function isLetter(c){
+  return c.toLowerCase() != c.toUpperCase();
+}
+
+function containsSpecialChar(username){
+  let specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if(specialChars.test( username+"")){
+      return true;
+    } else {
+      return false;
+    }
+}
+
+function validUsername(username) {
+  if(isLetter(username.charAt(0)) && isUserCorrectLength(username) && !containsSpecialChar(username))
+    return true;
+  else 
+    return false;
+}
+
+/* 
   Write a function that returns true or false if the given password
   is valid according to the following rules:
   - password must be between (and including) 10-64 characters in length
   - password must contain at least 1 letter, 1 number, and 1 special character
 */
+
+
+function isPassCorrectLength(password){
+  pass = password + ""
+  return pass.length >= 10 && pass.length <= 64;
+}
+
+function containsLetter(password){
+  pass = password + "";
+  var count = 0; 
+
+  for (var i = 0; i < pass.length; i++){
+    if (isLetter(pass.charAt(i)))
+      count++; 
+  }
+
+  if (count > 1)
+    return true; 
+}
+
+function containsNumber(password){
+  pass = password + "";
+  var count = 0; 
+
+  for (var i = 0; i < pass.length; i++){
+    if (!isNaN(pass.charAt(i)))
+      count++; 
+  }
+
+  if (count > 1)
+    return true; 
+  
+}
+
+
+
 function validPassword(password) {
-  return;
+  if(isPassCorrectLength(password) && containsLetter(password) && containsNumber(password) && containsSpecialChar(password))
+    return true;
+  else 
+    return false;
 }
 
 module.exports = { validUsername, validPassword };
